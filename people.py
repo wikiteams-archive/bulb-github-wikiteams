9e99c16cc957b4b8b435778b0d1f3dff417be3df
WikiTeams <info@wikiteams.pl>
2013-07-03 17:33:05 +0200

    model changes

--- people.py
+++ people.py
@@ -9,9 +9,20 @@ class Person(Node):
 
     element_type = "person"
 
-    name = String(nullable=False)
-    age = Integer()
+    name = String(nullable=True)
 
+class User(Person):
+    
+    element_type = "user"
+    
+    actor_attributes_gravatar_id = String(nullable=True)
+    actor_attributes_type = String(nullable=True)
+    actor_attributes_login = String(nullable=True)
+    actor_attributes_name = String(nullable=True)
+    actor_attributes_company = String(nullable=True)
+    actor_attributes_location = String(nullable=True)
+    actor_attributes_blog = String(nullable=True)
+    actor_attributes_email = String(nullable=True)
 
 class Knows(Relationship):
 
